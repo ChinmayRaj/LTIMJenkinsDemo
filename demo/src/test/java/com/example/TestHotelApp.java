079c26c9e9656f0caa813ce7b456ae3fb21f91b7
neojarvis <techadmin@iamneo.ai>
2024-12-28 04:25:49 +0000

    Auto-save-commit-12/28/2024--9:55:37-AM

---
+++ demo/src/test/java/com/example/TestHotelApp.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestHotelApp {
+    public static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        try{
+            ChromeOptions op=new ChromeOptions();
+            op.addArguments("--disable-notifications");
+
+     driver=new RemoteWebDriver(new URL("http://localhost:4444"),op);
+     driver.get("http://www.adactinhotelapp.com");
+     driver.findElement(By.id("username")).sendKeys("chinmay");
+     driver.findElement(By.id("password")).sendKeys("chinmay123");
+     driver.findElement(By.className("login_button")).click();
+
+     String msg=driver.findElement(By.className("auth_error")).getText();
+
+     System.out.println(msg);
+        }catch(MalformedURLException e){
+e.printStackTrace();
+        }
+        finally{
+            driver.quit();
+        }
+    }
+}
