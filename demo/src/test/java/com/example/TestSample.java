a593a1bd437323fa7b2faa18eaf00b5c5b300fff
neojarvis <techadmin@iamneo.ai>
2025-01-02 14:01:00 +0000

    Auto-save-commit-1/2/2025--7:30:44-PM

---
+++ demo/src/test/java/com/example/TestSample.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestSample {
+    public static WebDriver driver;
+    public static void main(String[] args)throws InterruptedException {
+        
+    }
+}
