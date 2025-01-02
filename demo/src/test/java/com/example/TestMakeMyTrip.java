0bcadde7f5d91a4cb83ea0d7ab6aacca54f30bdd
neojarvis <techadmin@iamneo.ai>
2025-01-02 04:32:55 +0000

    Manual-save-commit-1/2/2025--10:02:40-AM

--- demo/src/test/java/com/example/TestMakeMyTrip.java
+++ demo/src/test/java/com/example/TestMakeMyTrip.java
@@ -1,6 +1,30 @@
 package com.example;
 
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class TestMakeMyTrip {
-    
-    psvm
+    public static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException{
+        try {
+            ChromeOptions op=new ChromeOptions();
+            op.addArguments("--disable-notifications");
+            op.addArguments("--start-maximized");
+
+            driver=new RemoteWebDriver(new URL("http://localhost:4444"),op);
+     driver.get("https://www.makemytrip.com/");
+     Thread.sleep(2000);
+     
+     
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            driver.quit();
+        }
+    }
 }
