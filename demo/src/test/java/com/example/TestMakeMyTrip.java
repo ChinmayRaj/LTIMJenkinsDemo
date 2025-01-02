57ec14d9d7aad0268faa85aaf7f6c02e7a4a5481
neojarvis <techadmin@iamneo.ai>
2025-01-02 04:34:30 +0000

    Manual-save-commit-1/2/2025--10:04:15-AM

--- demo/src/test/java/com/example/TestMakeMyTrip.java
+++ demo/src/test/java/com/example/TestMakeMyTrip.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -17,8 +18,9 @@ public class TestMakeMyTrip {
             driver=new RemoteWebDriver(new URL("http://localhost:4444"),op);
      driver.get("https://www.makemytrip.com/");
      Thread.sleep(2000);
+     driver.findElement(By.xpath("//span[@class='commonModal_close']")).click();
      
-     
+
 
         } catch (Exception e) {
             e.printStackTrace();
