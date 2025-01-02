3ce2bda661471318a23781337e5f2a6badcaf152
neojarvis <techadmin@iamneo.ai>
2025-01-02 04:46:02 +0000

    Auto-save-commit-1/2/2025--10:15:46-AM

--- demo/src/test/java/com/example/TestMakeMyTrip.java
+++ demo/src/test/java/com/example/TestMakeMyTrip.java
@@ -4,7 +4,9 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestMakeMyTrip {
@@ -19,8 +21,18 @@ public class TestMakeMyTrip {
      driver.get("https://www.makemytrip.com/");
      Thread.sleep(2000);
      driver.findElement(By.xpath("//span[@class='commonModal_close']")).click();
-     
+     Thread.sleep(3000);
+     driver.findElement(By.xpath("//div/label/input[@id='fromCity']")).click();
+     Thread.sleep(2000);
+     driver.findElement(By.xpath("//div/label/input[@id='fromCity']")).sendKeys("Bhubaneswar");
+     Thread.sleep(2000);
+     Actions act=new Actions(driver);
 
+     WebElement btn1=driver.findElement(By.xpath("//div/p[test()='Bhubaneswar, India']"));
+       act.moveToElement(btn1).build().perform();
+       Thread.sleep(2000);
+       btn1.click();
+       Thread.sleep(3000);
 
         } catch (Exception e) {
             e.printStackTrace();
