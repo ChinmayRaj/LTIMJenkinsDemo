1eaf6f4523fb61cd8ee0554d49306af62c5ae6b1
neojarvis <techadmin@iamneo.ai>
2025-01-04 10:01:26 +0000

    Auto-save-commit-1/4/2025--3:31:09-PM

--- demo/src/test/java/com/example/TestScreenshot.java
+++ demo/src/test/java/com/example/TestScreenshot.java
@@ -3,27 +3,32 @@ package com.example;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestScreenshot {
     public static WebDriver driver;
     public static void main(String[] args)throws MalformedURLException,IOException{
-        // ChromeOptions op=new ChromeOptions();
-        // driver=new RemoteWebDriver(new URL("http://localhost:4444"), op);
-        // op.addArguments("--disable-notifications");
-        // driver.manage().window().maximize();
-        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        ChromeOptions op=new ChromeOptions();
+        driver=new RemoteWebDriver(new URL("http://localhost:4444"), op);
+        op.addArguments("--disable-notifications");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        // driver.get("https://www.jiomart.com");
+        driver.get("https://www.jiomart.com");
        
-        TestCreation tc=new TestCreation();
-       TestEnd te=new TestEnd();
+    //     TestCreation tc=new TestCreation();
+    //    TestEnd te=new TestEnd();
 
 
-       tc.OpenURL(driver);
+    //    tc.OpenURL(driver);
 
         File scrdir=new File("/home/coder/project/workspace/demo/screenshots/");
         if(!scrdir.exists()){
@@ -35,7 +40,8 @@ public class TestScreenshot {
        File dest=new File(scrdir,"screenshot.png");
         FileHandler.copy(src,dest);
         
-     te.TestEnd(driver);
+    //  te.TestEnd(driver);
+    driver.quit();
 
     } 
 }
