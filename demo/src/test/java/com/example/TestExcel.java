d3c42523402f9b931bcc6d9ed6ed044a62604883
neojarvis <techadmin@iamneo.ai>
2025-01-04 04:57:44 +0000

    Auto-save-commit-1/4/2025--10:27:27-AM

--- demo/src/test/java/com/example/TestExcel.java
+++ demo/src/test/java/com/example/TestExcel.java
@@ -1,25 +1,40 @@
 package com.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.bidi.module.Browser;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import com.google.common.annotations.VisibleForTesting;
+
 
 public class TestExcel {
 public static WebDriver driver;
 
 @BeforeMethod
-public void EstablisgConnection()throws MalformedURLException{
+public void EstablishConnection()throws MalformedURLException{
       ChromeOptions op=new ChromeOptions();
-      driver=new RemoteWebDriver(new java.net.URL("http://localhost:4444"), op);
-      
+      driver=new RemoteWebDriver(new URL("http://localhost:4444"), op);
+       op.addArguments("--disable-notifications");
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+       driver.get("https://www.jiomart.com");
 }
+@Test
+public void TestReadExcel()throws IOException{
+FileInputStream fis=new FileInputStream("/home/coder/project/workspace/demo/ExcelData/excelsheet.xlsx");
+
+XSSFWorkbook wb=new XSSFWorkbook(fis);
+}
+
 
 }
