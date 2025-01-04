640af44515f45b61040793b58834bb5caf10ede4
neojarvis <techadmin@iamneo.ai>
2025-01-04 05:12:44 +0000

    Auto-save-commit-1/4/2025--10:42:27-AM

--- demo/src/test/java/com/example/TestExcel.java
+++ demo/src/test/java/com/example/TestExcel.java
@@ -6,11 +6,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -34,6 +38,19 @@ public void TestReadExcel()throws IOException{
 FileInputStream fis=new FileInputStream("/home/coder/project/workspace/demo/ExcelData/excelsheet.xlsx");
 
 XSSFWorkbook wb=new XSSFWorkbook(fis);
+XSSFSheet sheet=wb.getSheetAt(0);
+Row row=sheet.getRow(0);
+Cell cel0=row.getCell(0);
+Cell cel1=row.getCell(1);
+
+System.out.println("First Name:"+cel0+" ,Last Name:"+cel1);
+}
+
+@AfterTest
+public void QuitDriver(){
+    if(driver!=null){
+          driver.quit();
+    }
 }
 
 
