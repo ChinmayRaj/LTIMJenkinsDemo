41747d07437c5d19a3c6caabb6c4f479c10eed4b
neojarvis <techadmin@iamneo.ai>
2025-01-05 05:28:38 +0000

    Auto-save-commit-1/5/2025--10:58:20-AM

---
+++ adactin/src/test/java/com/example/TestHybrid.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class TestHybrid {
+  
+    public static WebDriver driver;
+
+    @BeforeMethod
+    
+}
