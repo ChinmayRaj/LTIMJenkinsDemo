11c3ce6d42fd929fed09ecca763d4eee8754fc71
neojarvis <techadmin@iamneo.ai>
2025-01-05 06:28:38 +0000

    Auto-save-commit-1/5/2025--11:58:20-AM

--- adactin/src/test/java/com/example/TestHybrid.java
+++ adactin/src/test/java/com/example/TestHybrid.java
@@ -5,6 +5,10 @@ import java.time.Duration;
 import java.util.Properties;
 import java.net.URL;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,21 +18,17 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestHybrid {
-  public static String url;
-  public static String filePath;
+  
+  public static XSSFSheet sheet;
+  public static XSSFWorkbook wb;
+  
     public static WebDriver driver;
 public static By uname=By.id("username");
 public static By pass=By.id("password");
 public static By login=By.id("login");
     @BeforeMethod
     public void setup()throws IOException{
-        filePath=System.getProperty("user.dir");
-//taking url from config directory
-        FileInputStream file=new FileInputStream(filePath+"/config/config.properties");
-        Properties props=new Properties();
-        props.load(file);
-        url=props.getProperty("Url");
-
+      
         ChromeOptions op=new ChromeOptions();
         op.addArguments("--disable-notifications");
         driver=new RemoteWebDriver(new URL("http://localhost:4444"), op);
@@ -45,7 +45,7 @@ public static By login=By.id("login");
       th.typefield(uname, "Tonymontana");
       th.typefield(pass,"jarvis123");
       th.clickElement(login);
-      Thread.sleep(4000);
+      
     }
     @AfterTest
     public void tearDown(){
@@ -58,5 +58,20 @@ public static By login=By.id("login");
     public void clickElement(By loc){
         driver.findElement(loc).click();
     }
-    public static void setExcelFile(String excelpath,String SheetName)
+    public static void setExcelFile(String excelpath,String SheetName)throws IOException{
+       FileInputStream fis=new FileInputStream(excelpath);
+       wb=new XSSFWorkbook(fis);
+       sheet=wb.getSheet(SheetName);
+       XSSFRow row=sheet.getRow(0);
+       XSSFCell cell=row.getCell(0);
+
+    }
+    public static String readProp(String propToRead){
+        String  filePath=System.getProperty("user.dir");
+        FileInputStream file=new FileInputStream(filePath+"/config/config.properties");
+        Properties props=new Properties();
+        props.load(file);
+        String property=props.getProperty(propToRead);
+        return property;
+    }
 }
