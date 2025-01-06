76d494f655d4d2d7590d61bf194cd5fb013f5e73
neojarvis <techadmin@iamneo.ai>
2025-01-06 04:55:21 +0000

    Auto-save-commit-1/6/2025--10:25:03-AM

--- d2/src/test/java/com/example/AppTest1.java
+++ d2/src/test/java/com/example/AppTest1.java
@@ -2,6 +2,7 @@ package com.example;
 
 
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
@@ -10,17 +11,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 
-public class AppTest 
+public class AppTest1
 {
     public static WebDriver driver;
 
    @BeforeMethod
-   public void onSetup(){
+   public void onSetup()throws MalformedURLException{
        ChromeOptions op=new ChromeOptions();
        driver=new RemoteWebDriver(new URL("http://localhost:4444"), op);
        op.addArguments("--disable-notifications");
@@ -33,9 +35,10 @@ public class AppTest
     public void TestBigsmall()
     {
         Actions act=new Actions(driver);
-        
-     WebElement    christmas=driver.findElement(By.xpath("//ul/li[2]/details[@id='site-nav-item--2']"));
 
+     WebElement    christmas=driver.findElement(By.xpath("//ul/li[2]/details[@id='site-nav-item--2']"));
+act.moveToElement(christmas).build().perform();
+driver.findElement(By.partialLinkText("Secret")).click();
     }
     @AfterMethod
     public void close(){
