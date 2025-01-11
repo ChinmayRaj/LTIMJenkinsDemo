685d905bf18ea51e4552f4cafc3022d172bb7c83
neojarvis <techadmin@iamneo.ai>
2025-01-11 07:26:47 +0000

    Auto-save-commit-1/11/2025--12:56:22-PM

--- demo7/src/test/java/com/example/runner/TestRunner.java
+++ demo7/src/test/java/com/example/runner/TestRunner.java
@@ -5,8 +5,8 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features="/home/coder/project/workspace/demo7/features/HotelApp.feature",
-    glue="com.exapmle1.StepDefinitions",
+    features = "/home/coder/project/workspace/demo7/features/HotelApp.feature",
+    glue = "com.example.StepDefinitions",
     plugin={"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"}
 )
 public class TestRunner {
